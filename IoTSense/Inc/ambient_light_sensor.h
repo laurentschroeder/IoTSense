e3e62830deaa97022ccf7f25030eea2241f8d6d7
Laurent <Laurent@DESKTOP-IME5898.fritz.box>
2018-01-07 15:41:27 +0100

    changed ambient light sensor implementation

--- IoTSense/Inc/ambient_light_sensor.h
+++ IoTSense/Inc/ambient_light_sensor.h
@@ -8,72 +8,13 @@
 #ifndef AMBIENT_LIGHT_SENSOR_H_
 #define AMBIENT_LIGHT_SENSOR_H_
 
-#define SLAVE_ADDRESS 0x60
+void si1145_init();
+void si1145_reset();
+uint16_t si1145_getVisible();
+uint16_t si1145_getUVIndex();
+uint8_t si1145_getResponse();
+void si1145_sendNOP();
+char* si1145_getStatus();
 
-// registers
-#define COMMAND_REG     0x18
-#define PARAM_RD_REG    0x2E
-#define PARAM_WR_REG    0x17
-#define HW_KEY_REG      0x07
-#define RESPONSE_REG    0x20
-#define ALS_VIS_DATA0   0x22
-#define ALS_VIS_DATA1   0x23
-
-// commands
-#define PARAM_QUERY     0x80
-#define PARAM_SET       0xA0
-#define RESET           0x01
-#define ALS_FORCE       0x06
-
-// RAM address and parameters
-#define CHLIST          0x01    //adress of CHLIST
-#define EN_UV           0x80
-#define EN_AUX          0x40
-#define EN_ALS_IR       0x20
-#define EN_ALS_VIS      0x10
-#define EN_PS1          0x01
-
-#define ALS_VIS_ADC_COUNTER 0x10    //address of ALS_VIS_ADC_COUNTER
-//The recommended VIS_ADC_REC value is the one’s complement of ALS_VIS_ADC_GAIN.
-#define ADC_Clock_1    0x00    //50 ns times 2^ALS_VIS_ADC_GAIN
-#define ADC_Clock_7    0x10    //350 ns times 2^ALS_VIS_ADC_GAIN
-#define ADC_Clock_15   0x20    //750 ns times 2^ALS_VIS_ADC_GAIN
-#define ADC_Clock_31   0x30    //1.55 μs times 2^ALS_VIS_ADC_GAIN
-#define ADC_Clock_63   0x40    //3.15 μs times 2^ALS_VIS_ADC_GAIN
-#define ADC_Clock_127  0x50    //6.35 μs times 2^ALS_VIS_ADC_GAIN
-#define ADC_Clock_255  0x60    //12.75 μs times 2^ALS_VIS_ADC_GAIN
-#define ADC_Clock_511  0x70    //25.55 μs times 2^ALS_VIS_ADC_GAIN
-
-#define ALS_VIS_ADC_GAIN    0x11    //adress of ALS_VIS_ADC_GAIN
-#define ADC_CLOCK_DIV1      0x00
-#define ADC_CLOCK_DIV16     0x04
-#define ADC_CLOCK_DIV64     0x06
-#define ADC_CLOCK_DIV128    0x07
-
-#define ALS_VIS_ADC_MISC    0x12
-#define VIS_RANGE_NORMAL    0x00
-#define VIS_RANGE_HIGH      0x20
-
-/* private functions */
-
-static uint8_t ReadFromRegister(uint8_t reg);
-
-static void WriteToRegister(uint8_t reg, uint8_t value);
-
-static void ParamSet(uint8_t address, uint8_t value);
-
-static uint8_t ParamGet(uint8_t address);
-
-
-
-/* public functions */
-
-void reset();
-
-void init();
-
-uint16_t GetVisible();
-
-void SendNOP();
 
 #endif /* AMBIENT_LIGHT_SENSOR_H_ */
--- IoTSense/Src/ambient_light_sensor.c
+++ IoTSense/Src/ambient_light_sensor.c
@@ -8,7 +8,128 @@
 #include "stm32f0xx_hal.h"
 #include "ambient_light_sensor.h"
 
+#define SLAVE_ADDRESS 0x60
+
+// registers
+#define HW_KEY          0x07
+#define MEAS_RATE0      0x08
+#define MEAS_RATE1      0x09
+#define PS_LED21        0x0F
+#define PS_LED3         0x10
+#define UCOEF0          0x13
+#define UCOEF1          0x14
+#define UCOEF2          0x15
+#define UCOEF3          0x16
+#define PARAM_WR        0x17
+#define COMMAND         0x18
+#define RESPONSE        0x20
+#define ALS_VIS_DATA0   0x22
+#define ALS_VIS_DATA1   0x23
+#define ALS_IR_DATA0    0x24
+#define ALS_IR_DATA1    0x25
+#define PS1_DATA0       0x26
+#define PS1_DATA1       0x27
+#define UVINDEX0        0x2C
+#define UVINDEX1        0x2D
+#define PARAM_RD        0x2E
+#define CHIP_STAT       0x30
+
+// commands
+#define PARAM_QUERY     0x80
+#define PARAM_SET       0xA0
+#define RESET           0x01
+#define ALS_FORCE       0x06
+#define PSALS_FORCE     0x07
+#define GET_CAL         0x12
+#define NOP             0x00
+#define PS_FORCE        0x05
+
+// RAM address and parameters
+#define CHLIST          0x01
+#define EN_UV           0x80
+#define EN_AUX          0x40
+#define EN_ALS_IR       0x20
+#define EN_ALS_VIS      0x10
+#define EN_PS1          0x01
+
+#define PSLED12_SELECT  0x02
+#define LED1_ENABLE     0x01
+
+#define PS_ADC_COUNTER  0x0A
+#define PS_ADC_Clock_1     0x00    //50 ns times 2^PS_ADC_GAIN
+#define PS_ADC_Clock_7     0x10    //350 ns times 2^PS_ADC_GAIN
+#define PS_ADC_Clock_15    0x20    //750 ns times 2^PS_ADC_GAIN
+#define PS_ADC_Clock_31    0x30    //1.55 탎 times 2^PS_ADC_GAIN
+#define PS_ADC_Clock_63    0x40    //3.15 탎 times 2^PS_ADC_GAIN
+#define PS_ADC_Clock_127   0x50    //6.35 탎 times 2^PS_ADC_GAIN
+#define PS_ADC_Clock_255   0x60    //12.75 탎 times 2^PS_ADC_GAIN
+#define PS_ADC_Clock_511   0x70    //25.55 탎 times 2^PS_ADC_GAIN
+
+#define PS_ADC_GAIN     0x0B
+#define PS_ADC_CLOCK_DIV1   0x00
+#define PS_ADC_CLOCK_DIV16  0x04
+#define PS_ADC_CLOCK_DIV32  0x05
+
+#define PS_ADC_MISC     0x0C
+#define PS_RANGE_NORMAL 0<<5
+#define PS_RANGE_HIGH   1<<5
+#define PS_ADC_MODE_RAW 0<<2
+#define PS_ADC_MODE_PS  1<<2
+
+#define ALS_IR_ADCMUX   0x0E
+#define SMALL_IR        0x00
+#define LARGE_IR        0x03
+
+#define ALS_VIS_ADC_COUNTER 0x10
+#define VIS_ADC_Clock_1    0x00    //50 ns times 2^ALS_VIS_ADC_GAIN
+#define VIS_ADC_Clock_7    0x10    //350 ns times 2^ALS_VIS_ADC_GAIN
+#define VIS_ADC_Clock_15   0x20    //750 ns times 2^ALS_VIS_ADC_GAIN
+#define VIS_ADC_Clock_31   0x30    //1.55 ms times 2^ALS_VIS_ADC_GAIN
+#define VIS_ADC_Clock_63   0x40    //3.15 탎 times 2^ALS_VIS_ADC_GAIN
+#define VIS_ADC_Clock_127  0x50    //6.35 탎 times 2^ALS_VIS_ADC_GAIN
+#define VIS_ADC_Clock_255  0x60    //12.75 탎 times 2^ALS_VIS_ADC_GAIN
+#define VIS_ADC_Clock_511  0x70    //25.55 탎 times 2^ALS_VIS_ADC_GAIN
+
+#define ALS_VIS_ADC_GAIN    0x11
+#define VIS_ADC_CLOCK_DIV1      0x00
+#define VIS_ADC_CLOCK_DIV16     0x04
+#define VIS_ADC_CLOCK_DIV64     0x06
+#define VIS_ADC_CLOCK_DIV128    0x07
+
+#define ALS_VIS_ADC_MISC    0x12
+#define VIS_RANGE_NORMAL    0x00
+#define VIS_RANGE_HIGH      0x20
+
+#define ALS_IR_ADC_MISC		0x1F
+#define IR_RANGE_NORMAL		0x00
+#define IR_RANGE_HIGH		0x20
+
+#define ALS_IR_ADC_COUNTER  0x1D
+#define IR_ADC_Clock_1      0x00    //50 ns times 2^ALS_IR_ADC_GAIN
+#define IR_ADC_Clock_7      0x10    //350 ns times 2^ALS_IR_ADC_GAIN
+#define IR_ADC_Clock_15     0x20    //750 ns times 2^ALS_IR_ADC_GAIN
+#define IR_ADC_Clock_31     0x30    //1.55 탎 times 2^ALS_IR_ADC_GAIN
+#define IR_ADC_Clock_63     0x40    //3.15 탎 times 2^ALS_IR_ADC_GAIN
+#define IR_ADC_Clock_127    0x50    //6.35 탎 times 2^ALS_IR_ADC_GAIN
+#define IR_ADC_Clock_255    0x60    //12.75 탎 times 2^ALS_IR_ADC_GAIN
+#define IR_ADC_Clock_511    0x70    //25.55 탎 times 2^ALS_IR_ADC_GAIN
+
+#define ALS_IR_ADC_GAIN        0x1E
+#define IR_ADC_CLOCK_DIV1      0x00
+#define IR_ADC_CLOCK_DIV16     0x04
+#define IR_ADC_CLOCK_DIV64     0x06
+#define IR_ADC_CLOCK_DIV128    0x07
+
+#define ALS_IR_ADC_MISC     0x1F
+#define IR_RANGE_NORMAL     0x00
+#define IR_RANGE_HIGH       0x20    //Gain divided by 14.5
+
 extern I2C_HandleTypeDef hi2c1;
+
+static uint8_t ReadFromRegister(uint8_t reg);
+static void WriteToRegister(uint8_t reg, uint8_t value);
+static void ParamSet(uint8_t address, uint8_t value);
+static uint8_t ParamGet(uint8_t address);
 
 static uint8_t ReadFromRegister(uint8_t reg)
 {
@@ -24,50 +145,87 @@ static void WriteToRegister(uint8_t reg, uint8_t value)
 
 static void ParamSet(uint8_t address, uint8_t value)
 {
-    WriteToRegister(PARAM_WR_REG, value);
-    WriteToRegister(COMMAND_REG, PARAM_SET | address);
+    WriteToRegister(PARAM_WR, value);
+    WriteToRegister(COMMAND, PARAM_SET | address);
 }
 
 static uint8_t ParamGet(uint8_t address)
 {
-    WriteToRegister(COMMAND_REG, PARAM_QUERY | address);
-    uint8_t message = ReadFromRegister(PARAM_RD_REG);
+    WriteToRegister(COMMAND, PARAM_QUERY | address);
+    uint8_t message = ReadFromRegister(PARAM_RD);
     return message;
 }
 
-void reset()
+void si1145_reset()
 {
-    WriteToRegister(COMMAND_REG, RESET);
+    WriteToRegister(COMMAND, RESET);
 }
 
-void init()
+void si1145_init()
 {
-    WriteToRegister(HW_KEY_REG, 0x17);
+    WriteToRegister(HW_KEY, 0x17);
     HAL_Delay(150);
-    ParamSet(ALS_VIS_ADC_COUNTER, ADC_Clock_127);
-    ParamSet(ALS_VIS_ADC_GAIN, ADC_CLOCK_DIV128);
+    WriteToRegister(MEAS_RATE0,0x00);
+    WriteToRegister(MEAS_RATE1,0x00);
+    ParamSet(ALS_VIS_ADC_COUNTER, VIS_ADC_Clock_511);
+    ParamSet(ALS_VIS_ADC_GAIN, VIS_ADC_CLOCK_DIV128);
     ParamSet(ALS_VIS_ADC_MISC, VIS_RANGE_NORMAL);
-
+    ParamSet(ALS_IR_ADC_MISC, IR_RANGE_NORMAL);
+    WriteToRegister(UCOEF0, 0x7B);
+    WriteToRegister(UCOEF1, 0x6B);
+    WriteToRegister(UCOEF2, 0x01);
+    WriteToRegister(UCOEF3, 0x00);
+    //ParamSet(CHLIST, EN_ALS_VIS|EN_UV);
 }
 
-uint16_t GetVisible()
+void trigger()
 {
-    uint16_t data = 0;
-    ParamSet(CHLIST, EN_ALS_VIS);
-    WriteToRegister(COMMAND_REG, ALS_FORCE);
+    WriteToRegister(COMMAND, ALS_FORCE);
     HAL_Delay(150);
+}
+
+uint16_t si1145_getVisible()
+{
+    trigger();
+    uint16_t data = 0;
     uint8_t vis0 = ReadFromRegister(ALS_VIS_DATA0);
     uint8_t vis1 = ReadFromRegister(ALS_VIS_DATA1);
-    data = vis0 + (vis1 << 8);
+    data = vis0 | (uint16_t)(vis1 << 8);
     return data;
 }
 
-uint8_t GetResponse()
+uint16_t si1145_getUVIndex()
 {
-    return ReadFromRegister(RESPONSE_REG);
+    trigger();
+    uint8_t vis0 = ReadFromRegister(UVINDEX0);
+	uint8_t vis1 = ReadFromRegister(UVINDEX1);
+	uint16_t data = (uint16_t)vis0 | (vis1 << 8);
+	return data;
 }
 
-void SendNOP()
+uint8_t si1145_getResponse()
 {
-    WriteToRegister(COMMAND_REG, 0x00);
+    return ReadFromRegister(RESPONSE);
+}
+
+void si1145_sendNOP()
+{
+    WriteToRegister(COMMAND, NOP);
+}
+
+void calibrate()
+{
+
+}
+
+char* si1145_getStatus()
+{
+    uint8_t status = ReadFromRegister(CHIP_STAT);
+    switch(status)
+    {
+        case 1: return "SLEEP";
+        case 2: return "SUSPEND";
+        case 4: return "RUNNING";
+        default: return "ERROR";
+    }
 }
